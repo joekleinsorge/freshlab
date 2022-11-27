60c40bc8aa072ce40722184e093c1f7125e42e2c
joeykleinsorge <josephkleinsorge@gmail.com>
2022-11-26 22:07:02 -0500

    ci:commit

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 helm template \
-    --dependency-update \
     --include-crds \
     --namespace argocd \
-    argocd . \
+    bootstrap-argocd . -f values.yaml \
     | kubectl apply -n argocd -f -
 
 kubectl -n argocd wait --timeout=60s --for condition=Established \
-       crd/applications.argoproj.io \
-       crd/applicationsets.argoproj.io
+       crd/applications.argoproj.io
+
